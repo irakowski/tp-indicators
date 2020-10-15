00dbe47b2893c664de29d5a22230f5838f0ca361
irakowski <inna.rakowski@gmail.com>
2020-10-15 03:05:06 +0200

    CleanUp

--- tp/settings.py
+++ tp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
